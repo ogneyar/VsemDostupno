808a4c151720d47c139e54d4ab2e1c670e931bf2
ogneyar <ogneyar-ne@ya.ru>
2022-03-15 15:39:26 +0300

    eee

--- dump.bat
+++ dump.bat
@@ -1,6 +1,6 @@
 @echo off
 
-rem скачивание dump'a БД u1366210_bless
+rem скачивание dump'a БД bless
 
 "C:/Program Files/MySQL/MySQL Server 5.5/bin/mysqldump" --host=127.0.0.1 --password= -u root --disable-keys --add-drop-table --default-character-set=utf8 --result-file=bless.sql bless
 
