d6cd746d6d9b2cfac5c87c00d8c37f39a7a0ba9e
ogneyar <ogneyar-ne@ya.ru>
2022-04-01 20:36:49 +0300

    ууу

--- modules/site/views/layouts/main.php
+++ modules/site/views/layouts/main.php
@@ -31,6 +31,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/png', 'href' => '/imag
 <html lang="<?= Yii::$app->language ?>">
     <head>
         <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
