7505806ea2f38227f483eb0c502569e6615b3c70
ogneyar <ya13th@mail.ru>
2023-09-02 12:16:25 +0300

    real auto commit

--- dump.bat
+++ dump.bat
@@ -2,7 +2,7 @@
 
 rem скачивание dump'a БД bless
 
-"C:/Program Files/MySQL/MySQL Server 5.5/bin/mysqldump" --host=127.0.0.1 --password= -u root --disable-keys --add-drop-table --default-character-set=utf8 --result-file=bless.sql bless
+mysqldump --host=127.0.0.1 --password=root -u root --disable-keys --add-drop-table --default-character-set=utf8 --result-file=bless.sql bless
 
 echo =========================================
 
--- ldump.bat
+++ ldump.bat
@@ -2,7 +2,7 @@
 
 rem загрузка БД bless в MySQL
 
-"C:/Program Files/MySQL/MySQL Server 5.5/bin/mysql" --host=127.0.0.1 --password= -u root --default-character-set=utf8 bless < bless.sql
+mysql --host=127.0.0.1 --password=root -u root --default-character-set=utf8 bless < bless.sql
 
 echo =========================================
 
