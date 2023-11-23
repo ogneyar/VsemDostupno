9a89f62b494ba0239737765b6416ff588860a5e5
ogneyar <ya13th@mail.ru>
2023-11-23 14:52:36 +0300

    real auto commit

--- modules/bots/utils/listOfPurchases.php
+++ modules/bots/utils/listOfPurchases.php
@@ -1,13 +1,13 @@
 <?php
 
 
-// use app\models\Category;
+use app\models\Category;
 use app\models\CategoryHasProduct;
 use app\models\Product;
 use app\models\ProductFeature;
 use app\models\ProductPrice;
 // use app\models\ProviderHasProduct;
-// use app\models\Provider;
+use app\models\Provider;
 use app\modules\purchase\models\PurchaseProduct;
 
 
@@ -16,17 +16,83 @@ function listOfPurchases($bot, $from_id, $purchase_id, $step = 1) {
     
     $purchaseProduct = PurchaseProduct::findOne($purchase_id);
     // $bot->sendMessage($from_id, date('d.m.Y', $purchase_date));
-    $bot->sendMessage($from_id, date('d.m.Y', strtotime($purchaseProduct->purchase_date)));
+    // $bot->sendMessage($from_id, date('d.m.Y', strtotime($purchaseProduct->purchase_date)));
 
-    // $purchaseProducts = PurchaseProduct::find()->where(['provider_id' => $provider_id])->andWhere(['status' => 'advance'])->all();
-    // $quantity = 0;
-    // foreach($purchaseProducts as $purchaseProduct) {   
-    //     $productFeature = ProductFeature::findOne($purchaseProduct->product_feature_id);
-    //     $product_id = $productFeature->product_id;
-    //     $product = Product::findOne($product_id);
-    //     $productName = $product->name;
-    //     $productPrice = ProductPrice::findOne(['product_id' => $product_id]);
-    //     $categoryHasProduct = CategoryHasProduct::findOne(['product_id' => $product_id]);
+    if ($step == 1) {
+        $productFeature = ProductFeature::findOne($purchaseProduct->product_feature_id);
+        $product_id = $productFeature->product_id;
+        // $product = Product::findOne($product_id);
+        // $productName = $product->name;
+        // $productPrice = ProductPrice::findOne(['product_id' => $product_id]);
+        $provider_id = $purchaseProduct->provider_id;
+        $provider = Provider::findOne($provider_id);
+        $providerName = $provider->name;
+        $categoryHasProduct = CategoryHasProduct::findOne(['product_id' => $product_id]);
+        $category_id = $categoryHasProduct->category_id;
+        $category = Category::findOne($category_id);
+        $categoryName = $category->name;
+
+        $send = date('d.m.Y', strtotime($purchaseProduct->purchase_date)) . "г., состоится закупка " . $categoryName . " от " . $providerName;
+
+        $InlineKeyboardMarkup = [
+            'inline_keyboard' => [
+                // [
+                //     [
+                //         'text' => "Смотреть весь перечень товаров",
+                //         'callback_data' => 'listOfOroductsWithADescription_' . $product_id
+                //     ], 
+                // ],      
+                [
+                    [
+                        'text' => "Масло семечек тыквы 0.5л – 240.00 / 195.00",
+                        'callback_data' => 'listOfOroductsWithAphoto_' . $product_id
+                    ],
+                ],      
+                [
+                    [
+                        'text' => "Масло семечек арбуза 0.5л. – 240.00 / 195.00",
+                        'callback_data' => 'listOfOroductsWithAphoto_' . $product_id
+                    ],
+                ],                
+            ]
+        ];
+
+        $bot->sendMessage($from_id, $send, null, $InlineKeyboardMarkup);
+    }
+
+    
+    // $allCategories = [];
+    // foreach($products as $product) {
+    //     $feature_id = $product->product_feature_id;
+    //     $product_feature = ProductFeature::findOne($feature_id);
+    //     $real_product_id = $product_feature->product_id;
+    //     $real_product = Product::findOne($real_product_id);
+    //     if ($real_product->visibility == 0) continue;
+    //     $categoryHasProduct = CategoryHasProduct::findOne(['product_id' => $real_product_id]);
+    //     $category_id = $categoryHasProduct->category_id;
+    //     $category = Category::findOne($category_id);
+    //     $yes = false;
+    //     foreach($allCategories as $oneCategory) {
+    //         if ($oneCategory['category_id'] == $category_id && $oneCategory['purchase_date'] == strtotime($product->purchase_date)) $yes = true;
+    //     }
+    //     if ( ! $yes ) $allCategories[] = [
+    //         'category_id' => $category_id, 
+    //         'category_name' => $category->name,
+    //         'purchase_id' => $product->id, 
+    //         'purchase_date' => strtotime($product->purchase_date), 
+    //     ];
+    // }
+
+
+    $purchaseProducts = PurchaseProduct::find()->where(['purchase_date' => $purchaseProduct->purchase_date])->andWhere(['status' => 'advance'])->all();
+    $quantity = 0;
+    foreach($purchaseProducts as $purchaseProduct) {   
+        // $productFeature = ProductFeature::findOne($purchaseProduct->product_feature_id);
+        // $product_id = $productFeature->product_id;
+        // $product = Product::findOne($product_id);
+        // $productName = $product->name;
+        // $productPrice = ProductPrice::findOne(['product_id' => $product_id]);
+        // $categoryHasProduct = CategoryHasProduct::findOne(['product_id' => $product_id]);
     //     if ($categoryHasProduct->category_id == $category_id && $product->visibility) {
     //         $quantity++;
     //         if ($quantity <= (($step - 1)*4)) continue;
@@ -46,7 +112,7 @@ function listOfPurchases($bot, $from_id, $purchase_id, $step = 1) {
 
     //         $bot->sendMessage($from_id, $send, null, $InlineKeyboardMarkup);
     //     }
-    // }
+    }
         
     // if ($quantity > ($step*4)) {        
     //     $step++;
