a9b391a3a1723c7c7e8040d2b6cb3341373b63db
ogneyar <ya13th@mail.ru>
2024-01-13 16:49:47 +0300

    real auto commit

--- modules/bots/utils/search/searchProducts.php
+++ modules/bots/utils/search/searchProducts.php
@@ -15,6 +15,8 @@ function searchProducts($bot, $tg_id, $search) {
     }
 
     $found = false;
+    // $arrayPurchases = [];
+    $inline_keyboard = [];
 
     foreach ($purchaseProducts as $purchaseProduct) {
         $feature_id = $purchaseProduct->product_feature_id;
@@ -22,18 +24,30 @@ function searchProducts($bot, $tg_id, $search) {
         $product_id = $productFeature->product_id;
         $product = Product::findOne($product_id);
         $productName = $product->name;
-
+        
         if (mb_strpos(mb_strtolower($productName), mb_strtolower($search), 0, "UTF-8") !== false) {
-            $send = "Урраааа нашёл!!!";
-            $bot->sendMessage($tg_id, $send);
-            $found = true;
-            break;
+            // array_push($arrayPurchases, $purchaseProduct);
+            if ($product->visibility) {
+                $found = true;
+                $inline_keyboard[] = [
+                    [
+                        'text' => $productName,
+                        'callback_data' => 'productWithAPhoto_' . $productFeature->id
+                    ],
+                ];                
+            }
+            // $send = "Урраааа нашёл!!!";
+            // $bot->sendMessage($tg_id, $send);
+            // break;
         }
     }
 
     if ( ! $found ) {
-        $send = "Не нашёл :'(";
+        $send = "Ничего не найдено по искомому запросу:\r\n\r\n" . $search;
         $bot->sendMessage($tg_id, $send);
+        return;
     }
 
+    $InlineKeyboardMarkup = [ 'inline_keyboard' => $inline_keyboard, ];
+    $bot->sendMessage($tg_id, $search, null, $InlineKeyboardMarkup);
 }
